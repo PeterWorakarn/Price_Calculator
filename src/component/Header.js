1c0cb04d8c18845f915e432443e5888155a28beb
Peter Oun <57410426+PeterWorakarn@users.noreply.github.com>
2021-06-04 10:39:59 +0700

    Fix CSS bug

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header className="bg-brand max-w-full">
             <section className="max-w-6xl mx-auto py-4 px-2 sm:p-4">
-                <div className="sm:h-32 ml-2  sm:p-4  flex items-center">
+                <div className="sm:h-32 mt-2 ml-2  sm:p-4  flex items-center">
                     <img className="w-1/6 h-1/6 sm:w-auto sm:h-auto rounded-full ring-4 ring-white mr-6 sm:mr-8" src={avatar} alt="avatar" />
                     <Typing />
                 </div>
